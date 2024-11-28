b95fe41b7b49260683cf459bf3fb7c3c93106af9
Diakhate-mohamade <dxate2016@gmail.com>
2024-11-28 18:41:30 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'licence_gestion_notes_frontEnd';
+}
